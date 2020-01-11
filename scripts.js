2c44a557c01b2e930ebb2020670a4c83efaa2e6f
Clinton-Davis <clintongadavis@gmail.com>
2020-01-11 00:52:27 +0000

    almost conpleated challange 3

--- scripts.js
+++ scripts.js
@@ -276,9 +276,12 @@ console.log(isDesigner);
 */
 
 /*
-John and his family went on a holiday and went to 3 different restaurants. The bills were $124, $48 and $268.
+John and his family went on a holiday and went to 3 different restaurants. 
+The bills were $124, $48 and $268.
 To tip the waiter a fair amount, John created a simple tip calculator (as a function).
-He likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+He likes to tip 20% of the bill when the bill is less than $50, 
+15% when the bill is between $50 and $200, and 
+10% if the bill is more than $200.
 In the end, John would like to have 2 arrays:
 1) Containing all three tips (one for each bill)
 2) Containing all three final paid amounts (bill + tip).
@@ -286,15 +289,25 @@ In the end, John would like to have 2 arrays:
 GOOD LUCK 😀
 */
 
-
+var totalTips = [];
+var totalBill = []
 function tipCalclator(bill) {
     if (bill < 50) {
-        0.2 * bill;
+        totalTips.push(bill * 0.2);
     } else if (bill > 50 && bill < 200) {
-        0.15 * bill;
-    } else 0.1 * bill;
+        totalTips.push(bill * 0.15);
+    } else {
+        totalTips.push(bill * 0.1);
+    }
 }
-console.log(tipCalclator(124));
+tipCalclator(124);
+tipCalclator(48);
+tipCalclator(268);
+console.log(totalTips);
+console.log(totalBill);
+
+
+
 
 
 
