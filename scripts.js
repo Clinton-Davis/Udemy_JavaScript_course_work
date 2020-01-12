499661acf211a2375dd8219d175a67e3cdbd4c00
Clinton Davis <you@example.com>
2020-01-12 23:21:52 +0000

    just adding challange 4

--- scripts.js
+++ scripts.js
@@ -341,7 +341,7 @@ console.log(tips, finalValues);
  *  var name = {}; or var name = new object();
  *  Key: Value,
  */
-
+// Object litural syntex
 var clint = {
 	firstName: "Clint",
 	lastName: "Davis",
@@ -362,9 +362,41 @@ clint.job = "teacher";
 clint["firstName"] = "Clinton";
 console.log(clint);
 
+// New Object syntes
 var sinead = new Object();
 sinead.firstName = "sinead";
 sinead.lastName = "Davis";
 sinead.birthYear = 1985;
 sinead[job] = "Nerse";
 console.log(sinead);
+
+/***************************************************8
+ * Objects and Methods
+ */
+
+var clint = {
+	firstName: "Clint",
+	lastName: "Davis",
+	birthYear: 1975,
+	family: ["Sinead", "Kitty"],
+	job: "Disigner",
+	isMarried: true,
+	calcAge: function(birthYear) {
+		this.age = 2018 - this.birthYear;
+	}
+};
+clint.calcAge();
+console.log(clint);
+
+/*****************************
+ * CODING CHALLENGE 4
+ */
+
+/*
+Let's remember the first coding challenge where Mark and John compared their BMIs. Let's now implement the same functionality with objects and methods.
+1. For each of them, create an object with properties for their full name, mass, and height
+2. Then, add a method to each object to calculate the BMI. Save the BMI to the object and also return it from the method.
+3. In the end, log to the console who has the highest BMI, together with the full name and the respective BMI. Don't forget they might have the same BMI.
+Remember: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
+GOOD LUCK 😀
+*/
